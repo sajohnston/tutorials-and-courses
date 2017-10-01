269738ad0a9cc54b153dfaf15d5d437d980feb34
Sarah Johnston <sarahj27@gmail.com>
2017-10-01 21:57:12 +0100

    First octave video

---
+++ machinelearning.m
@@ -0,0 +1,23 @@
+1 ~= 2 % not
+% ; suppresses output
+a = pi;
+disp(a) % displays the values
+disp(sprintf('2 decimals: %0.2f', a))
+
+A = [1 2; 3 4; 5 6]
+
+V = [1; 2; 3]
+V2 = 1:0.1:2
+V2'
+
+C = 2*ones(2,3)
+
+w = rand(3,3)
+wnorm = randn(1, 3)
+
+w = -6 + sqrt(10)*(randn(1,10000));
+hist(w)
+
+eye(4) % 4x4 'eye'dentity matrix
+
+% help function name

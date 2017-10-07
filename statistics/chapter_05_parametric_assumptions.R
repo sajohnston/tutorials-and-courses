e2e9a7f3752afa3b29af2d47df6ac903a7c3bd35
Sarah Johnston <sarahj27@gmail.com>
2017-10-07 17:08:27 +0100

    Added Q-Q plots

--- statistics/chapter_05_parametric_assumptions.R
+++ statistics/chapter_05_parametric_assumptions.R
@@ -16,3 +16,16 @@ hist.day2 + addNormalCurve(dlf$day2, na.rm = TRUE)
 
 hist.day3 <-plotDensity(dlf, "day3", "Hygiene score on day 3")
 hist.day3 + addNormalCurve(dlf$day3, na.rm = TRUE)
+
+## The book says to use qplot with stat = "qq" but the stat arg is deprecated
+qqplot.day1 <- qplot(sample = dlf$day1)
+qqplot.day1
+
+qqplot.day1 <-   ggplot(dlf, aes(sample = dlf$day1)) + stat_qq() 
+qqplot.day1
+
+qqplot.day2 <- ggplot(dlf, aes(sample = dlf$day2)) + stat_qq() 
+qqplot.day2
+
+qqplot.day3 <- ggplot(dlf, aes(sample = dlf$day3)) + stat_qq() 
+qqplot.day3

533446ddb9c05fa34c520514ce815ce6cfc99d81
Sarah Johnston <sarahj27@gmail.com>
2018-05-05 13:07:07 +0100

    Faster fibonacci, added F45 to test

--- AlgorithmsToolbox/AlgorithmsToolbox/fibonacci.py
+++ AlgorithmsToolbox/AlgorithmsToolbox/fibonacci.py
@@ -4,4 +4,14 @@ def fibonacci(n):
     if n <= 1:
         return n
     
-    return fibonacci(n-1) + fibonacci(n-2)
+    fibs = [0, 1]
+    for i in range(2, n+1):
+        nextFib = fibs[i-1] + fibs[i-2]
+        fibs.append(nextFib)
+
+    return fibs[n]
+
+
+if __name__ == "__main__":
+    n = int(input())
+    print(fibonacci(n))
--- AlgorithmsToolbox/AlgorithmsToolbox/test_Fibonacci.py
+++ AlgorithmsToolbox/AlgorithmsToolbox/test_Fibonacci.py
@@ -3,12 +3,10 @@ import fibonacci as f
 
 class Test_Fibonacci(unittest.TestCase):
     def test_accuracy(self):
-        n = [0, 1, 10]
-        Fn = [0, 1, 55]
+        n = [0, 1, 10, 45]
+        Fn = [0, 1, 55, 1134903170]
         for i in range(0, len(n)):
             assert f.fibonacci(n[i]) == Fn[i]
-
-
 
 
 if __name__ == '__main__':
